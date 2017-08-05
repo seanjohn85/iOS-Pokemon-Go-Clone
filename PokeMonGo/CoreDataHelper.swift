6d3b7320b83b798e344e1ca409c9a0c92b788180
John <johnkenny1985@gamil.com>
2017-08-05 13:32:17 +0100

    created core data helper

--- PokeMonGo/CoreDataHelper.swift
+++ PokeMonGo/CoreDataHelper.swift
@@ -9,11 +9,38 @@
 import UIKit
 import CoreData
 
-func createPoke(){
+func addallPoke(){
+    
+    create(name: "mew")
+    create(name: "snorlax")
+    create(name: "pidgey")
+    create(name: "pikachu-2")
+    create(name: "charmander")
+    create(name: "caterpie")
+    create(name: "meowth")
+    create(name: "dratini")
+    create(name: "psyduck")
+    create(name: "rattata")
+    create(name: "mankey")
+    create(name: "zubat")
+    create(name: "squirtle")
+    create(name: "abra")
+    create(name: "weedle")
+    create(name: "venonat")
+    create(name: "bullbasaur")
+    create(name: "eevee")
+    create(name: "bellsprout")
+    
+    
+    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+    
+}
+
+func create(name : String){
     let ctx = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     let pokemon = Pokemon(context: ctx)
-    pokemon.name = "Mew"
-    pokemon.imageName
-
+    pokemon.name = name.capitalized
+    pokemon.imageName = name
+    
     
 }
