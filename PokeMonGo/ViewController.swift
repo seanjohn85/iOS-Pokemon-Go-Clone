17bb1bb102feadd17f1e9574bd2e2b565205ecef
John <johnkenny1985@gamil.com>
2017-08-06 00:50:41 +0100

    fully functional pokemon go clone

--- PokeMonGo/ViewController.swift
+++ PokeMonGo/ViewController.swift
@@ -35,24 +35,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         manager.delegate = self
         
         if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
-            //shows the users location
-            map.showsUserLocation = true
-            //update the manager with the users location
-            manager.startUpdatingLocation()
-            //set a timer intervel to run every 5 seconds
-            Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
-                //span pokemon
-                if let cord = self.manager.location?.coordinate{
-                    //add anotation to the map
-                    let annotation = PokeAnno(coor: cord, poke: self.pokemen[Int(arc4random_uniform(UInt32(self.pokemen.count)))])
-        
-                    annotation.coordinate.latitude += (Double(arc4random_uniform(200)) - 100.0) / 40000.0
-                    annotation.coordinate.longitude += (Double(arc4random_uniform(200)) - 100.0) / 40000.0
-                    //add the annotation to the map
-                    self.map.addAnnotation(annotation)
-                }
-                
-            })
+            setUp()
+            
         }else{
             //request user permission
             manager.requestWhenInUseAuthorization()
@@ -126,11 +110,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             map.setRegion(region, animated: true)
             Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: { (timer) in
                 if let cord = self.manager.location?.coordinate{
+                    //get the selected pokemon
+                    let pokemon = (view.annotation as! PokeAnno).pokemon
                     //check if the user is still in display
                     if MKMapRectContainsPoint(self.map.visibleMapRect, MKMapPointForCoordinate(cord)){
                         print("in view")
-                        //get the selected pokemon
-                        let pokemon = (view.annotation as! PokeAnno).pokemon
+                        let alert = UIAlertController(title: "Yaay😀", message: "You have cought \(pokemon.name!).", preferredStyle: .alert)
+                        let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                        alert.addAction(ok)
+                        let dex = UIAlertAction(title: "Pokedex", style: .default, handler: { (action) in
+                            self.performSegue(withIdentifier: "dex", sender: nil)
+                        })
+                        alert.addAction(dex)
+                        self.present(alert, animated: true, completion: nil)
                         //update core data to cot for this pokemon
                         pokemon.cot = true
                         (UIApplication.shared.delegate as! AppDelegate).saveContext()
@@ -138,12 +130,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
                         self.map.removeAnnotation(view.annotation!)
                         
                     }else{
-                        print("to far away")
+                        let alert = UIAlertController(title: "Woops", message: "You are 2 far away to catch \(pokemon.name!). Please move closer☺️", preferredStyle: .alert)
+                        let ok = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                        alert.addAction(ok)
+                        self.present(alert, animated: true, completion: nil)
                     }
                 }
             })
             
         }
+    }
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse{
+            setUp()
+        }
+    }
+    
+    func setUp(){
+        //shows the users location
+        map.showsUserLocation = true
+        //update the manager with the users location
+        manager.startUpdatingLocation()
+        //set a timer intervel to run every 5 seconds
+        Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+            //span pokemon
+            if let cord = self.manager.location?.coordinate{
+                //add anotation to the map
+                let annotation = PokeAnno(coor: cord, poke: self.pokemen[Int(arc4random_uniform(UInt32(self.pokemen.count)))])
+                
+                annotation.coordinate.latitude += (Double(arc4random_uniform(200)) - 100.0) / 40000.0
+                annotation.coordinate.longitude += (Double(arc4random_uniform(200)) - 100.0) / 40000.0
+                //add the annotation to the map
+                self.map.addAnnotation(annotation)
+            }
+            
+        })
+        
     }
 
 
